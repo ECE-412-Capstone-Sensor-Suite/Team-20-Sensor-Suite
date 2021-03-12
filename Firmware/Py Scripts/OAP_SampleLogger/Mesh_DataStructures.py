74968b1d33cbfdd1e0358562a3090b8a4e426c59
Yooooos <72731760+Yooooos@users.noreply.github.com>
2021-03-11 16:01:46 -0800

    MESH NETWORK DATA structure, USE TESTING.py to see how structure works

---
+++ Firmware/Py Scripts/OAP_SampleLogger/Mesh_DataStructures.py
@@ -0,0 +1,97 @@
+from datetime import datetime
+class samples: # Sample Object structure
+    def __init__(self, timeStr, temp, humid, lux, wind, accel):
+        self.temp = temp
+        self.humid = humid
+        self.lux = lux
+        self.wind = wind
+        self.accel = accel
+        self.timeStr = timeStr
+
+
+class Mote(samples): # mote Object structure : inherits from samples
+    def __init__(self, MAC):
+        self.MAC = MAC
+
+    def LoadMote(self, logfile): # load a single mote from logfile into ram, using motes MAC address
+        self.temp = []
+        self.humid = []
+        self.lux = []
+        self.wind = []
+        self.accel = []
+        self.timeStr = []
+        for lines in logfile.readlines():
+            words = lines.split()
+            MACaddr = ''
+            if len(words) > 5:
+                MACaddr = words[3][1:-2]
+
+            if self.MAC == MACaddr:
+                self.timeStr.append(words[0])
+                temperature = ''.join(filter(lambda i: i.isdigit(), words[5]))
+                self.temp.append(int(temperature))
+                #print 'Temp: ' + str(temperature)
+
+                Humid = ''.join(filter(lambda i: i.isdigit(), words[6]))
+                self.humid.append(int(Humid))
+                #print 'Humid: ' + str(Humid)
+
+                Lux = ''.join(filter(lambda i: i.isdigit(), words[7]))
+                self.lux.append(int(Lux))
+                #print 'LUX: ' + str(Lux)
+
+                wind = ''.join(filter(lambda i: i.isdigit(), words[8]))
+                self.wind.append(int(wind))
+                #print 'wind: ' + wind
+
+                axel = ''
+                for i in range(9, len(words)):
+                    axel = axel + words[i]
+                XYZaccel = ''.join(filter(lambda i: i.isdigit(), axel))
+                XYZ = []
+                for i in range(0, len(XYZaccel) / 3):
+                    XYZ.append((int(XYZaccel[i * 3]), int(XYZaccel[i * 3 + 1]), int(XYZaccel[i * 3 + 2])))
+                #print 'accel: ' + str(XYZ)
+                self.accel.append(XYZ)
+        logfile.seek(0)
+
+    def timestamp(self, hour, min):
+        hours = str(hour)
+        mins = str(min)
+        if hour < 10:
+            hours = '0' + hours
+        if min < 10: mins = '0' + mins
+        testTime = hours + ':' + mins
+        for i in range(len(self.timeStr)):
+            if testTime[0:5] == self.timeStr[i][0:5]:
+                print 'timestamp found! --> ' + self.timeStr[i]
+                return samples(self.timeStr[i], self.temp[i], self.humid[i], self.lux[i], self.wind[i], self.accel[i])
+
+
+class MeshNetwork(Mote): # Mesh Network object structure : inherits from mote
+    def __init__(self):
+        self.Motes = []
+    def loadMesh(self, logfile): # find unique adresses, define mote object with them, and load each one using loadMote
+        uniqAddr = []
+        addresses = []
+        for lines in logfile.readlines():
+            words = lines.split()
+            MACaddr = ''
+            if len(words) > 5:
+                MACaddr = words[3][1:-2]
+            addresses.append(MACaddr)
+        [uniqAddr.append(x) for x in addresses if x not in uniqAddr]
+        uniqAddr.reverse()
+        uniqAddr = uniqAddr[0:-1]
+        logfile.seek(0)
+
+        print '\nNumber of motes: ' + str(len(uniqAddr))
+        for i in range(len(uniqAddr)):
+            print 'mote ' + str(i) + ' ---> MAC: ' + uniqAddr[i]
+            self.Motes.append(Mote(uniqAddr[i])) # define mote object using MAC
+            self.Motes[i].LoadMote(logfile)      # load mote into ram
+
+    def moteMAC(self, MACaddress):
+        for i in range(len(self.Motes)):
+            if self.Motes[i].MAC == MACaddress:
+                return self.Motes[i]
---
+++ Firmware/Py Scripts/OAP_SampleLogger/testing.py
@@ -0,0 +1,47 @@
+from Mesh_DataStructures import MeshNetwork
+from Mesh_DataStructures import Mote
+
+# LOADING SAMPLES INTO MESH / MOTE STRUCTURE
+readLog = open('spoofed sampleLog.log', 'r')
+
+mote1 = Mote('00-17-0d-00-00-32-23-d3')    # define mote object with mac address = '00-17-0d-00-00-32-23-d3'
+mote1.LoadMote(readLog)                    # load logfile contents related mote1 and store in mote1 object
+
+Mesh = MeshNetwork()        # define MeshNetwrok Object
+Mesh.loadMesh(readLog)      # load Log file contents for ALL motes within int Mesh object, Prints out mote MACs and IDs
+
+readLog.close()
+
+# MeshNetwork indexing: Mesh= [mote2, mote1, ... ], mote1 = [mac, temp,  humidity ...], temp = [sample0, sample1, ...]
+# to access smaple1 from mote0: ---> sample1 =  Mesh.Motes[0].temp[1]
+print '\nPrint 3 Temp and Humidity samples from each mote'
+for mote in Mesh.Motes:
+    print 'Mote: ' + mote.MAC
+    for i in range(3):
+        print 'Temp: ' + str( mote.temp[i] ) + ' & ' + 'Humidity: ' + str( mote.humid[i] ) + ' @ ' + mote.timeStr[i]
+
+# MoteID and Timestamp Indexing
+print '\nCalling sample from specific mote at specific time using MoteID and timeStamp:'
+sample = Mesh.Motes[3].timestamp(18,3) # store all samples at time 18:03
+
+print float(sample.humid)/100               # SAMPLES ARE LOADED AS INTEGERS SO THEY CAN BE MANIPULATED AS NUMBERS !!!!
+print str( sample.wind ) + ' mph'
+print str( float(sample.temp)/100 ) + 'C'
+print str( sample.lux ) + ' lux'
+
+# Calling all sample of a single type as an array:
+print '\nAll temp readings from Mote 2'
+Temps = Mesh.Motes[2].temp
+print Temps
+
+# if you dont know MoteID for specific mote:
+print '\nCalling samples from specific mote at specific time using Mac Address and timeStamp:'
+MacSample = Mesh.moteMAC('00-17-0d-00-00-32-23-d3').timestamp(19,10)   # store all samples at time 18:03
+print 'temp = ' + str(float(MacSample.temp)/100) + 'C' + ' @ ' + MacSample.timeStr
+
+
+
+
+    #for index in range(1,3):
+        #print moteI.temp[0]
+        #print 'temp: ' + str( moteI.temp[index] ) + ' & ' + 'Humidity: ' + str( moteI.humid[index] ) + ' @ ' + moteI.timeStr[index]
